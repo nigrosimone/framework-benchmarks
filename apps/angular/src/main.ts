01721ad4c89ead3bd87c45ec782349301ba2a80b
Nigro Simone <nigro.simone@gmail.com>
2026-06-29 07:29:46 +0200

    chore: removed other default

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -1,11 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideZonelessChangeDetection(),
-    provideHttpClient()
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent).catch(err => console.error(err));
